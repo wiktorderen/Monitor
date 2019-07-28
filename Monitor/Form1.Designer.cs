23ccc232b922893aedddcdd6f2a7eb83d16df02a
Wiktor <wiktor1474@wp.pl>
2019-07-28 17:36:36 +0200

    new functions added

--- Monitor/Form1.Designer.cs
+++ Monitor/Form1.Designer.cs
@@ -36,11 +36,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(44, 27);
+            this.label1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Location = new System.Drawing.Point(24, 32);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(176, 31);
@@ -51,7 +57,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(44, 87);
+            this.label2.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Location = new System.Drawing.Point(24, 137);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 17);
@@ -67,7 +74,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(250, 190);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(251, 190);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 17);
@@ -79,7 +87,7 @@
             // 
             this.comboBox1.BackColor = System.Drawing.SystemColors.MenuText;
             this.comboBox1.DisplayMember = "10";
-            this.comboBox1.ForeColor = System.Drawing.Color.Gold;
+            this.comboBox1.ForeColor = System.Drawing.Color.White;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "10",
@@ -89,7 +97,8 @@
             "50",
             "60",
             "70"});
-            this.comboBox1.Location = new System.Drawing.Point(382, 34);
+            this.comboBox1.Location = new System.Drawing.Point(366, 20);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(63, 24);
             this.comboBox1.TabIndex = 3;
@@ -98,30 +107,97 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(286, 41);
+            this.label4.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label4.Location = new System.Drawing.Point(251, 27);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.Size = new System.Drawing.Size(94, 17);
             this.label4.TabIndex = 4;
-            this.label4.Text = "Choice value";
+            this.label4.Text = "Choose value";
             this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label5.Location = new System.Drawing.Point(24, 83);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Graphics processor inromation";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label6.Location = new System.Drawing.Point(254, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Choose value";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.comboBox2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90"});
+            this.comboBox2.Location = new System.Drawing.Point(366, 74);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(63, 24);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(398, 282);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label7.Location = new System.Drawing.Point(24, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Network activity";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(457, 228);
+            this.ClientSize = new System.Drawing.Size(669, 367);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Gold;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(461, 232);
+            this.MinimumSize = new System.Drawing.Size(461, 230);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Monitor";
@@ -139,6 +215,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Monitor/Form1.cs
+++ Monitor/Form1.cs
@@ -9,7 +9,9 @@ namespace Monitor
     {
         PerformanceCounter perfCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
         PerformanceCounter perfSystemCounter = new PerformanceCounter("System", "System Up Time");
-
+        PerformanceCounter perfGPUCounter = new PerformanceCounter("GPU Engine", "Utilization Percentage", "SUM(pid_*_luid_0x00000000_0x000095CA_phys_*_eng_*_engtype_VideoEncode)");
+        PerformanceCounter perfNetworkD = new PerformanceCounter("Karta sieciowa", "Bajty odebrane/s", "Intel[R] Dual Band Wireless-AC 3165");
+        PerformanceCounter perfNetworkU = new PerformanceCounter("Karta sieciowa", "Bajty wysłane/s", "Intel[R] Dual Band Wireless-AC 3165");
 
         public Form1()
         {
@@ -20,18 +22,32 @@ namespace Monitor
         {
             float time = perfSystemCounter.NextValue();
 
-            int percent = (int)perfCPUCounter.NextValue();
+            int c = (int)perfCPUCounter.NextValue();
+
+           float nD = perfNetworkD.NextValue();
+            float nU = perfNetworkU.NextValue();
+
+            //int g = (int)perfGPUCounter.NextValue();
 
             int threshold;
-            threshold = Convert.ToInt32(comboBox1.SelectedItem); 
-            
-            label1.Text = "CPU:" + "                        " + percent + "  " + "%";
-            label2.Text = "System Up Time:" + "      " + change(time);
-            label3.Text = "Author:    Wiktor Dereń";
-            
+            threshold = Convert.ToInt32(comboBox1.SelectedItem);
 
-            if (percent > threshold)
+            int Gthreshold;
+            Gthreshold = Convert.ToInt32(comboBox2.SelectedItem);
+            
+            label1.Text = "CPU:" + "                        " + c + "  " + "%";
+            label2.Text = "System Up Time:" + "      " + change(time);
+            //label3.Text = "Author:    Wiktor Dereń";
+            //label5.Text = "GPU:" + "            " + g + "  " + "%";
+            label7.Text = "Network activity" + '\n'  +"Download:"+"              " +nD+"  mb/s"+ "    " + "    " +"Upload:"+ "   "  +       nU+"  mb/s";
+
+
+
+            if (c > threshold)
                 Console.Beep(5000, 500);
+
+            /*if (g > Gthreshold)
+                Console.Beep(1000, 250);*/
         }
         private string change(float time)
         {
@@ -42,6 +58,13 @@ namespace Monitor
                 return Math.Floor(time / 60) + " min " + Math.Floor((time/60-Math.Floor(time/60))*60) + " sec";
             
         }
+       /* private string change1(float nD, float nU)
+        {
+            if(nD>8)
+            {
+
+            }
+        }*/
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -64,12 +87,32 @@ namespace Monitor
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
             
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
-}//zmiana
+}
